0f1adf448aa255f72975659ba7ce391d973c95cb
Mayuresh Itankar <flame191835@gmail.com>
2026-03-30 12:13:59 +0530

    commit 1

---
+++ src/main/java/com/example/amazonlite/AmazonLiteApplication.java
@@ -0,0 +1,12 @@
+package com.example.amazonlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonLiteApplication.class, args);
+    }
+}
