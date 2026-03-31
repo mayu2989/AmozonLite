8e03224ddb7417f7a585682781527dbde7fd517d
Mayuresh Itankar <flame191835@gmail.com>
2026-03-31 16:47:33 +0530

    Added ApiResponse

--- src/main/java/com/example/amazonlite/repository/CartRepository.java
+++ src/main/java/com/example/amazonlite/repository/CartRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart,String> {
-    Optional<Cart> findByUserIdAndDeletedFalse(String userId);
+    Optional<Cart> findByUserIdAndIsDeletedFalse(String userId);
     Boolean existsByUserIdAndIsDeletedFalse(String userId);
 }
